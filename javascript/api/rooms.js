02b574589f53cf6d0d96dd7f13e8ce7b1c2d9ef9
nabellaayug <nabellaayugiwanti@gmail.com>
2023-12-06 16:07:12 +0700

    connect FE & BE part 1

--- javascript/api/rooms.js
+++ javascript/api/rooms.js
@@ -1,15 +1,34 @@
 export async function getRoom() {
-    try {
-      const response = await fetch("http://localhost:3000/room", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      return await response.json().data;
-    } catch (error) {
-      console.log(error);
-      return null;
-    }
+  try {
+    const response = await fetch("http://localhost:3000/room", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const {data} = await response.json()
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
   }
-  
+}
+
+export async function getRoomDetail(room_id) {
+  try {
+    const response = await fetch(`http://localhost:3000/room/detail?room_id=${room_id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const {data} = await response.json()
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
